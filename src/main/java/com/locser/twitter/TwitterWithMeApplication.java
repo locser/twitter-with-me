0bd51467967e1bf389b5906d192d04062cbf5118
locser <ploc280301@gmail.com>
2022-12-29 21:10:31 +0700

    init project and connect database postgres

---
+++ src/main/java/com/locser/twitter/TwitterWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.locser.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locser/twitter/TwitterWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locser.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
